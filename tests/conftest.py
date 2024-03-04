530f65ea779ab00f25ff4e7e39ab60b97ed9d33b
HaleySchuhl <haleyschuhl@gmail.com>
2024-03-04 09:36:35 -0600

    Update conftest.py
    add small rgb image for annotation test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,23 @@
+import os
+import pytest
 import matplotlib
 
 # Disable plotting
 matplotlib.use("Template")
+
+
+class TestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
+        # Flat image directory
+        self.snapshot_dir = os.path.join(self.datadir, "snapshot_dir")
+        # RGB image
+        self.small_rgb_img = os.path.join(self.datadir, "setaria_small_plant_rgb.png")
+
+
+@pytest.fixture(scope="session")
+def test_data():
+    """Test data object for the main PlantCV package."""
+    return TestData()
