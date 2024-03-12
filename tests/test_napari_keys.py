57c480fcfd3b79c0c3f368b8cde17dd486662587
Malia Gehan <mgehan@danforthcenter.org>
2024-03-12 17:07:02 -0500

    made napari_label_classes more general
    Made napari_label_classes more general, should work with all datatypes that napari_open can use although it might not make sense to label data in this way.

--- tests/test_napari_keys.py
+++ tests/test_napari_keys.py
@@ -1,21 +1,23 @@
 import numpy as np
-from plantcv.annotate.napari_keys import napari_keys
+from plantcv.annotate import napari_keys
 
 
 def test_napari_keys(qtbot, viewer_widget):
     """Test for PlantCV.Annotate"""
     # Read in test data
-    viewer = viewer_widget    
-    img = np.zeros((100,100))
-    coor = [(25,25), (50,50)]
+    viewer = viewer_widget
+    img = np.zeros((100, 100))
+    coor = [(25, 25), (50, 50)]
     viewer.add_image(img)
-    viewer.add_points(np.array(coor), symbol="o", name="total", face_color="red", size=30)
-    viewer.add_points(np.array(coor), symbol="o", name="test", face_color="red", size=30)
+    viewer.add_points(np.array(coor), symbol="o", name="total",
+                      face_color="red", size=30)
+    viewer.add_points(np.array(coor), symbol="o", name="test",
+                      face_color="red", size=30)
     keys = napari_keys(viewer)
 
     def check_keys():
         assert keys != []
 
-    qtbot.waitUntil(check_keys, timeout=60_000)   
+    qtbot.waitUntil(check_keys, timeout=60_000)
 
     assert keys == ['total', 'test']
--- tests/test_napari_label_classes.py
+++ tests/test_napari_label_classes.py
@@ -1,28 +1,20 @@
-import pytest
 import numpy as np
 from plantcv.annotate import napari_label_classes
 from plantcv.plantcv import readimage
 
 
-def test_napari_label_classes_rgb(qtbot, test_data):
-    """Test for PlantCV.Annotate"""
-    # Read in test data
-    img, _, _ = readimage(test_data.small_rgb_img)
-    with pytest.raises(Exception):
-        viewer = napari_label_classes(img,['background','seed'])    
-
-    
 def test_napari_label_classes_gray(qtbot, test_data):
     """Test for PlantCV.Annotate"""
     # Read in test data
     img, _, _ = readimage(test_data.kmeans_seed_gray_img)
-    viewer = napari_label_classes(img,['seed'])    
-    coor = [(25,25)]
-    viewer.add_points(np.array(coor), symbol="o", name='background', face_color="red", size=1)
-    def check_open():
-        assert np.shape(viewer.layers['background'].data) != (0,2)
+    viewer = napari_label_classes(img, ['seed'])
+    coor = [(25, 25)]
+    viewer.add_points(np.array(coor), symbol="o", name='background',
+                      face_color="red", size=1)
 
-    qtbot.waitUntil(check_open, timeout=60_000)   
+    def check_open():
+        assert np.shape(viewer.layers['background'].data) != (0, 2)
+
+    qtbot.waitUntil(check_open, timeout=60_000)
 
     assert len(viewer.layers['background'].data) == 1
-    
