82faaef4eaa0c5e3cfe0d79ed93c8fbda6bd58c9
Noah Fahlgren <noahfahlgren@gmail.com>
2024-03-28 18:00:16 -0500

    Add dummy test for napari viewier

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -4,4 +4,8 @@ from plantcv import annotate as an
 def test_annotate():
     """PlantCV Test"""
     assert an.__name__ == "plantcv.annotate"
-    
+
+
+def test_napari(make_napari_viewer):
+    """PlantCV Test"""
+    viewer = make_napari_viewer(show=False)
