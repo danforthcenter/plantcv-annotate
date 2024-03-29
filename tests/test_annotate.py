8093b981213e91661b8b4db32e909f14c9f9e03a
Noah Fahlgren <noahfahlgren@gmail.com>
2024-03-28 21:33:15 -0500

    make viewer with napari

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -1,11 +1,13 @@
 import numpy as np
+import napari
 from plantcv.annotate import napari_open
 
 
 def test_napari(qtbot):
     """PlantCV Test"""
     img = np.zeros((100, 100, 3), dtype=np.uint8)
-    viewer = napari_open(img, show=False)
+    # viewer = napari_open(img, show=False)
+    viewer = napari.view_image(img, show=False)
     coor = [(25, 25), (75, 75)]
     viewer.add_points(np.array(coor), symbol="o", name="total", face_color="red", size=1)
 
