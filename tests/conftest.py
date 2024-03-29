885d35a3b922c6c201ad60748e6fcc531e0daf61
Malia Gehan <mgehan@danforthcenter.org>
2024-03-29 11:03:22 -0500

    tests
    updating tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,39 @@
+import pytest
 import matplotlib
+import os
+import napari
+from typing import Callable
+
 
 # Disable plotting
 matplotlib.use("Template")
+
+
+class TestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                    "testdata")
+        # RGB image
+        filename_rgb = "setaria_small_plant_rgb.png"
+        self.small_rgb_img = os.path.join(self.datadir, filename_rgb)
+        # Kmeans Clustered Gray image
+        filename_kmeans = "silphium_seed_labeled_example.png"
+        self.kmeans_seed_gray_img = os.path.join(self.datadir, filename_kmeans)
+        # Small Hyperspectral image
+        filename_hyper = "corn-kernel-hyperspectral.raw"
+        self.envi_sample_data = os.path.join(self.datadir, filename_hyper)
+
+
+@pytest.fixture
+def viewer_widget(make_napari_viewer: Callable[..., napari.Viewer]):
+    viewer = make_napari_viewer(show=False)
+
+    return viewer
+
+
+@pytest.fixture(scope="session")
+def test_data():
+    """Test data object for the main PlantCV package."""
+    return TestData()
--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -3,17 +3,13 @@ import napari
 from plantcv.annotate import napari_open
 
 
-def test_napari(qtbot):
+def test_napari_open(viewer_widget):
     """PlantCV Test"""
     img = np.zeros((100, 100, 3), dtype=np.uint8)
     # viewer = napari_open(img, show=False)
-    viewer = napari.view_image(img, show=False)
+    viewer = viewer_widget
+    viewer = napari_open(img, viewer)
     coor = [(25, 25), (75, 75)]
     viewer.add_points(np.array(coor), symbol="o", name="total", face_color="red", size=1)
-
-    # def check_open():
-    #     assert np.shape(viewer.layers['total'].data) != (0, 2)
-
-    # qtbot.waitUntil(check_open, timeout=60_000)
 
     assert len(viewer.layers["total"].data) == 2
