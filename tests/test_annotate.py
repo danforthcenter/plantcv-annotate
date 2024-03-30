d256921d1b488fa390dbab04f0df4f037f72b194
Malia Gehan <mgehan@danforthcenter.org>
2024-03-29 19:53:31 -0500

    Update test_annotate.py

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -1,6 +1,5 @@
 import numpy as np
-#import napari
-#from plantcv.annotate import napari_open
+from plantcv.annotate import napari_open
 
 
 def test_napari_open(make_napari_viewer):
@@ -8,7 +7,7 @@ def test_napari_open(make_napari_viewer):
     img = np.zeros((100, 100, 3), dtype=np.uint8)
     # viewer = napari_open(img, show=False)
     viewer = make_napari_viewer(show=False)
-    # viewer = napari_open(img, viewer)
+    viewer = napari_open(img, viewer)
     # coor = [(25, 25), (75, 75)]
     # viewer.add_points(np.array(coor), symbol="o", name="total", face_color="red", size=1)
 
