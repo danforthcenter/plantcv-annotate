5082ad1dd03961a53d32ba85f92f1eddc252765c
Malia Gehan <mgehan@danforthcenter.org>
2024-04-02 15:50:24 -0500

    Update test_napari_join_labels.py
    fixed test

--- tests/test_napari_join_labels.py
+++ tests/test_napari_join_labels.py
@@ -1,5 +1,6 @@
 import numpy as np
 from plantcv.annotate.napari_open import napari_open
+from plantcv.annotate.napari_label_classes import napari_label_classes
 from plantcv.plantcv import readimage
 from plantcv.annotate.napari_join_labels import napari_join_labels
 
@@ -8,14 +9,7 @@ def test_napari_join_labels(test_data):
     """Test for PlantCV.Annotate"""
     # Read in test data
     img, _, _ = readimage(test_data.kmeans_seed_gray_img)
-    viewer = napari_open(img, show=False)
-    background = [(4, 143), (77, 246)]
-    viewer.add_points(np.array(background), symbol="o", name='background',
-                      face_color="red", size=1)
-    wing = [(275, 54)]
-    viewer.add_points(np.array(wing), symbol="o", name='wing',
-                      face_color="red", size=1)
-
+    viewer = napari_label_classes(img, ["seed"], show=False)
     labeled, _ = napari_join_labels(img, viewer)
 
     assert np.shape(labeled) == (576, 537)
