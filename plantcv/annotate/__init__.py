44819f6fb41cb4bb13f210cb13d2114305af4ad9
Malia Gehan <mgehan@danforthcenter.org>
2024-04-11 16:55:11 -0500

    napari save

--- plantcv/annotate/__init__.py
+++ plantcv/annotate/__init__.py
@@ -2,11 +2,13 @@ from plantcv.annotate.napari_classes import napari_classes
 from plantcv.annotate.napari_open import napari_open
 from plantcv.annotate.napari_label_classes import napari_label_classes
 from plantcv.annotate.napari_join_labels import napari_join_labels
+from plantcv.annotate.napari_save_coor import napari_save_coor
 
 
 __all__ = [
     "napari_classes",
     "napari_open",
     "napari_label_classes",
-    "napari_join_labels"
+    "napari_join_labels",
+    "napari_save_coor"
 ]
