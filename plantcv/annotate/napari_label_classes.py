4a1e5fcbbeace020b80c545abb0b7c5b772943be
Malia Gehan <mgehan@danforthcenter.org>
2024-05-09 13:10:13 -0500

    Update napari_label_classes.py
    updating napari label classes to allow import of data, also updating to allow size parameter and to change color instead of shape.

--- plantcv/annotate/napari_label_classes.py
+++ plantcv/annotate/napari_label_classes.py
@@ -3,9 +3,11 @@
 import numpy as np
 import random
 from plantcv.annotate import napari_open
+from plantcv.annotate import napari_classes
 
 
-def napari_label_classes(img, classes, show=True):
+def napari_label_classes(img, classes=False, size=10, shape='square',
+                         importdata=False, show=True):
     """
     open img in napari and label classes
 
@@ -19,6 +21,10 @@ def napari_label_classes(img, classes, show=True):
         is run. If all classes have points labeled, any clusters not labeled
         will default to the last class in the list when napari_join_labels is
         run.
+    size = size of marker in pixels
+    shape = either 'square' or 'circle'
+    importdata = dictionary of values in Napari format (y,x).
+        Output of napari_read_coor
     show = if show is True the viewer is launched. This opetion is useful for
     running tests without triggering the viewer.
 
@@ -26,18 +32,40 @@ def napari_label_classes(img, classes, show=True):
     viewer  = Napari viewer object
 
     :param img: numpy.ndarray
+    :param classes: list
+    :param size: int
+    :param shape: str
+    :param importdata: dict
+    :param show: str
     :return viewer: napari viewer object
 
     """
     showcall = show
     viewer = napari_open(img, show=showcall)
 
-    symbols = ['arrow', 'clobber', 'cross', 'diamond', 'disc', 'hbar', 'ring',
-               'square', 'star', 'tailed_arrow', 'triangle_down',
-               'triangle_up', 'vbar']
+    color = ['red', 'blue', 'purple', 'green', 'orange', 'yellow', 'cyan',
+             'magenta']
 
-    for x in classes:
-        viewer.add_points(np.array([]), name=x, symbol=random.choice(symbols),
-                          face_color='white', size=10)
+    keys = napari_classes(viewer)
+
+    if classes is not False:
+        for x in classes:
+            if x in keys:
+                pass
+            else:
+                viewer.add_points(np.array([]), name=x, symbol=shape,
+                                  face_color=random.choice(color), size=size)
+        keys = napari_classes(viewer)
+
+    if importdata is not False:
+        importkeys = list(importdata.keys())
+        for key in importkeys:
+            if len(importdata[key]) != 0:
+                if key in keys:
+                    viewer.layers[key].add(importdata[key])
+                else:
+                    viewer.add_points(importdata[key], name=key, symbol=shape,
+                                      face_color=random.choice(color),
+                                      size=size)
 
     return viewer
