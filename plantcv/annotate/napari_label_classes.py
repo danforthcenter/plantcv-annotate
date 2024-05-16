be7d2f93c25cceca03926f5bed64a7cbf781419a
Malia Gehan <mgehan@danforthcenter.org>
2024-05-16 12:11:40 -0500

    more colors
    adding all of the colors

--- plantcv/annotate/napari_label_classes.py
+++ plantcv/annotate/napari_label_classes.py
@@ -43,17 +43,46 @@ def napari_label_classes(img, classes=False, size=10, shape='square',
     showcall = show
     viewer = napari_open(img, show=showcall)
 
-    color = ['red', 'blue', 'purple', 'green', 'orange', 'yellow', 'cyan',
-             'magenta']
+    color = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure",
+             "beige", "bisque", "black", "blanchedalmond", "blue",
+             "blueviolet", "brown", "burlywood", "cadetblue",
+             "chartreuse", "chocolate", "coral", "cornflowerblue",
+             "cornsilk", "crimson", "cyan", "darkblue", "darkcyan",
+             "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki",
+             "darkmagenta", "darkolivegreen", "darkorange", "darkorchid",
+             "darkred", "darksalmon", "darkseagreen", "darkslateblue",
+             "darkslategray", "darkturquoise", "darkviolet", "deeppink",
+             "deepskyblue", "dimgray", "dodgerblue", "firebrick",
+             "floralwhite", "forestgreen", "fuchsia", "gainsboro",
+             "ghostwhite", "gold", "goldenrod", "gray", "green",
+             "greenyellow", "honeydew", "hotpink", "indianred",
+             "indigo", "ivory", "khaki", "lavender", "lavenderblush",
+             "lawngreen", "lemonchiffon", "lightblue", "lightcoral",
+             "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen",
+             "lightpink", "lightsalmon", "lightseagreen", "lightskyblue",
+             "lightslategray", "lightsteelblue", "lightyellow", "lime",
+             "limegreen", "linen", "magenta", "maroon", "mediumaquamarine",
+             "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
+             "mediumslateblue", "mediumspringgreen", "mediumturquoise",
+             "mediumvioletred", "midnightblue", "mintcream", "mistyrose",
+             "moccasin", "navajowhite", "navy", "oldlace", "olive",
+             "olivedrab", "orange", "orangered", "orchid", "palegoldenrod",
+             "palegreen", "paleturquoise", "palevioletred", "papayawhip",
+             "peachpuff", "peru", "pink", "plum", "powderblue", "purple",
+             "red", "rosybrown", "royalblue", "saddlebrown",
+             "salmon", "sandybrown", "seagreen", "seashell", "sienna",
+             "silver", "skyblue", "slateblue", "slategray", "snow",
+             "springgreen", "steelblue", "tan", "teal", "thistle", "tomato",
+             "turquoise", "violet", "wheat", "white", "whitesmoke",
+             "yellow", "yellowgreen"]
 
     keys = napari_classes(viewer)
 
     if classes is not False:
         for x in classes:
-            if x in keys:
-                pass
-            else:
+            if x not in keys:
                 viewer.add_points(np.array([]), name=x, symbol=shape,
+                                  edge_color=random.choice(color),
                                   face_color=random.choice(color), size=size)
         keys = napari_classes(viewer)
 
@@ -65,6 +94,7 @@ def napari_label_classes(img, classes=False, size=10, shape='square',
                     viewer.layers[key].add(importdata[key])
                 else:
                     viewer.add_points(importdata[key], name=key, symbol=shape,
+                                      edge_color=random.choice(color),
                                       face_color=random.choice(color),
                                       size=size)
 
