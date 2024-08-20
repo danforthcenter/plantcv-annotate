1afcbe066f3ab915a43a72bf7a1826cdad79e4ec
Malia Gehan <mgehan@danforthcenter.org>
2024-08-20 12:24:03 -0500

    docstring fix
    fixed docstring

--- plantcv/annotate/classestomove.py
+++ plantcv/annotate/classestomove.py
@@ -5,7 +5,5 @@ import napari
 
 class Viewer(napari.Viewer):
     def layer_size(self, layer):
-        """
-        method to get current size of points
-        """
+        """method to get current size of points"""
         return self.layers[layer]._current_size
