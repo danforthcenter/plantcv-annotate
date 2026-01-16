e43869d79db3fb5026e44953c6a5d38be58ef8c9
k034b363 <k034b363@gmail.com>
2026-01-16 16:32:36 -0600

    add bounding box function

---
+++ plantcv/annotate/napari_bbox.py
@@ -0,0 +1,27 @@
+# Create a bounding box for ROI or SAM using a napari viewer object
+
+
+def napari_bbox(viewer, layername):
+    """
+    Converts napari rectangles into bounding boxes for ROIs and SAM models
+
+    Inputs:
+    viewer  = napari viewer object
+    layername = the name of the shapes layer
+
+    Returns:
+    boxes = list of the bounding box parameters
+
+    :param viewer: napari.viewer.Viewer
+    :param layername: str
+    :return boxes: list
+    """
+    boxes = []
+    for i in viewer.layers[layername].data:
+        xs = []
+        ys = []
+        for j in i:
+            xs.append(int(j[1]))
+            ys.append(int(j[0]))
+        boxes.append([min(xs), min(ys), max(ys) - min(ys), max(xs) - min(xs)])
+    return boxes
